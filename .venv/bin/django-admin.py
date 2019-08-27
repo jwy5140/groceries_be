a8635b6a5623063f37d51fc41a6c52856119b188
jwy5140 <jwy5140@gmail.com>
2019-08-27 16:59:54 -0400

    init python backend

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/c/Users/Jeff/Desktop/Coding/Flatiron Proj/projects/groceries/be/.venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
